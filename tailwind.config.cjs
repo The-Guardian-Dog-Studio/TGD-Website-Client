6d5694b51b0c316f3ed3ad0d4d12296a7609917c
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-04 00:03:34 -0300

    chore: add tailwind color in the theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,24 @@
+import colors from "tailwindcss/colors";
+delete colors.lightBlue;
+delete colors.blueGray;
+delete colors.coolGray;
+delete colors.trueGray;
+delete colors.warmGray;
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    fontFamily: {
-      sans: ["Nunito", "sans-serif"],
-      display: ["Orbitron"],
-    },
-    colors: {
-      "brand-red": "#bf013d",
-      "brand-blue": "#184287",
-    },
-    extend: {},
-  },
-  plugins: [],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		fontFamily: {
+			sans: ["Nunito", "sans-serif"],
+			display: ["Orbitron"],
+		},
+		colors: {
+			...colors,
+			"brand-red": "#bf013d",
+			"brand-blue": "#184287",
+		},
+		extend: {},
+	},
+	plugins: [],
 };
