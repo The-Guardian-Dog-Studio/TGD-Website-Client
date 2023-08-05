898b5b3fdb7ede14319846776de7f21504740d12
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-04 23:22:22 -0300

    feat: create Heading component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from "./header";
+export { default as Heading } from "./heading";
 export { default as SocialMedia } from "./social-media";
 export * from "./icons";
 export * from "./sections";
