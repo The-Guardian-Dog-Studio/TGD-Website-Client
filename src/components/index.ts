ea1824cc7a27ae6441fb4c4dcb6864f78cf2ba23
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-04 23:45:05 -0300

    feat: add button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as Button } from "./button";
 export { default as Header } from "./header";
 export { default as Heading } from "./heading";
 export { default as SocialMedia } from "./social-media";
