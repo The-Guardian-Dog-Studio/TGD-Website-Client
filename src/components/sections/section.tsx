cc8d7a23d171e835d5fe6f47bfca2a1918fe8b45
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-07 12:54:24 -0300

    feat: add line under section

--- src/components/sections/section.tsx
+++ src/components/sections/section.tsx
@@ -2,12 +2,15 @@ interface SectionProps extends React.HTMLAttributes<HTMLDivElement> { }
 
 const Section = ({ children, ...props }: SectionProps): JSX.Element => {
 	return (
-		<section
-			{...props}
-			className={`mx-auto mt-14 flex w-11/12 max-w-screen-xl flex-col items-center gap-10 overflow-x-hidden ${props.className}`}
-		>
-			{children}
-		</section>
+		<>
+			<section
+				{...props}
+				className={`mx-auto mt-12 flex w-11/12 max-w-screen-xl flex-col items-center gap-10 overflow-x-hidden py-14 ${props.className}`}
+			>
+				{children}
+			</section>
+			<hr className="border-gray-400" />
+		</>
 	);
 };
 
