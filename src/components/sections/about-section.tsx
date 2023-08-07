5a887ce4fd4e6f2f5ea3dfd66c84f9484df33a66
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-07 12:55:31 -0300

    feat: add about section

---
+++ src/components/sections/about-section.tsx
@@ -0,0 +1,42 @@
+import { Button, Heading, Section } from "@/components";
+import { Tabs } from "@/components/ui";
+import type { AboutProtocol } from "@/domain/protocols/about";
+import data from "@/data.json";
+
+const aboutData: AboutProtocol = data["about"];
+
+const AboutSection = (): JSX.Element => {
+  return (
+    <Section id="about" className="gap-16">
+      <Heading level={1}>Sobre Nós</Heading>
+      <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-24">
+        <img
+          src={aboutData.banner.url}
+          alt={aboutData.banner.alt}
+          className="h-fit w-full object-contain"
+        />
+        <Tabs
+          tabs={aboutData.tabs.map((item) => item.title)}
+          content={aboutData.tabs.map((item) => (
+            <>
+              <p className="text-justify">{item.content}</p>
+              {typeof item.link !== "undefined" && (
+                <a
+                  href={item.link.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="mt-4 block text-end"
+                  aria-label={`Este link abre uma nova aba no navegador. Sua rota é: ${item.link.url}`}
+                >
+                  <Button tabIndex={-1}>{item.link.text}</Button>
+                </a>
+              )}
+            </>
+          ))}
+        />
+      </div>
+    </Section>
+  );
+};
+
+export default AboutSection;
