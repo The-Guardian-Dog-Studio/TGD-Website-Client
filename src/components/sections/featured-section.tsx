b1f521ded07b73fafcd8353eef4b4ae330ed1aad
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-08 10:24:21 -0300

    fix(featured-section.tsx): resolve url with prefix and consequently avoid unloaded image

--- src/components/sections/featured-section.tsx
+++ src/components/sections/featured-section.tsx
@@ -1,16 +1,22 @@
 import { Image, Link } from "@/components";
 import siteData from "@/data.json";
 import type { FeaturedGame } from "@/domain/protocols/featured-game";
+import { removeDoubleSlashes } from "@/utils";
 
 const featuredSectionData: FeaturedGame = siteData["featured-game"];
 
 const FeaturedSection = (): JSX.Element => {
+	const backgroundUrl = removeDoubleSlashes(
+		featuredSectionData.bannerUrl,
+		false
+	);
+
 	return (
 		<section
 			id="featured-game"
 			className="relative mt-[-5rem] grid h-screen w-full place-items-center"
 			style={{
-				background: `url(${featuredSectionData.bannerUrl})`,
+				background: `url(${backgroundUrl})`,
 				backgroundSize: "cover",
 				backgroundRepeat: "no-repeat",
 				backgroundPosition: "center",
@@ -21,7 +27,7 @@ const FeaturedSection = (): JSX.Element => {
 					<Image
 						className="h-24 w-full"
 						src="/gamepad-with-arrow.svg"
-						alt="O controle de videogame com uma seta apontando para baixo"
+						alt="O controle de videogame com uma seta apontando para baixo, indicando que irá te direcionar para a seção de jogos."
 					/>
 				</Link>
 			</div>
