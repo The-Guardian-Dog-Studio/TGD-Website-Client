591302ac9e8b84d67addb3e0b1775711131ac942
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-08 10:12:58 -0300

    feat: create responsive header

---
+++ src/components/header/menu/tailwind-classes.tsx
@@ -0,0 +1,11 @@
+export const links = (isActive: boolean) => {
+	return isActive
+		? "bg-brand-red text-white"
+		: "bg-transparent text-brand-blue";
+};
+
+export const icons = (isActive: boolean) => {
+	return isActive
+		? "translate-x-0 fill-white"
+		: "translate-x-2 fill-brand-blue";
+};
