577619b8aafa677f14acdad1ef4b04233a95d626
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-11 18:23:46 -0300

    feat: create quote component

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,5 +5,6 @@ export * from "./icons";
 export { default as Image } from "./image";
 export * from "./layout";
 export { Link } from "./link";
+export { default as Quote } from "./quote";
 export * from "./sections";
 export { default as SocialMedia } from "./social-media";
