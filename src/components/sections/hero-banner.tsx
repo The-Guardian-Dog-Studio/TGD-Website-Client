d468e1d3e827938460e57eef903f18bcadba773b
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-18 12:11:26 -0300

    refactor: change featured-section to hero-banner

---
+++ src/components/sections/hero-banner.tsx
@@ -0,0 +1,58 @@
+import { Image, Link } from "@/components";
+import { removeDoubleSlashes } from "@/utils";
+
+interface FeaturedSectionProps
+  extends React.ComponentPropsWithoutRef<"section"> {
+  id: string;
+  banner: {
+    url: string;
+  };
+  scrollDownButton?: {
+    active: boolean;
+    anchor?: string;
+  };
+}
+
+const HeroBanner = ({
+  id,
+  banner,
+  scrollDownButton,
+  children,
+  ...props
+}: FeaturedSectionProps): JSX.Element => {
+  const backgroundUrl = removeDoubleSlashes(banner.url, false);
+
+  return (
+    <section
+      id={id}
+      className="relative mt-[-5rem] grid h-screen w-full place-items-center"
+      style={{
+        background: `url(${backgroundUrl})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+      {...props}
+    >
+      {scrollDownButton?.active && (
+        <div className="absolute bottom-10 left-1/2 h-32 w-24 translate-x-[-50%]">
+          <Link
+            href={scrollDownButton.anchor}
+            noAnimation
+            title="Veja os nossos jogos"
+          >
+            <Image
+              className="h-24 w-full"
+              src="/gamepad-with-arrow.svg"
+              alt="O controle de videogame com uma seta apontando para baixo, indicando que irá te direcionar para a seção de jogos."
+            />
+          </Link>
+        </div>
+      )}
+
+      {children}
+    </section>
+  );
+};
+
+export default HeroBanner;
