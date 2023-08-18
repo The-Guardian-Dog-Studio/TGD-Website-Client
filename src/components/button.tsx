1ebf682beb20d7fd23fae1aafa6dabe43f97c00c
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-08-18 12:17:32 -0300

    feat(button.tsx): add color variants

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,12 +1,35 @@
 import { ButtonHTMLAttributes } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+const variants = {
+  primary: {
+    background: "bg-brand-red",
+    shadow: "shadow-brand-red",
+  },
+  secondary: {
+    background: "bg-amber-500",
+    shadow: "shadow-amber-500",
+  },
+};
 
-const Button = ({ children, ...props }: ButtonProps): JSX.Element => {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: keyof typeof variants;
+  noShadow?: boolean;
+}
+
+const Button = ({
+  children,
+  variant = "primary",
+  noShadow,
+  ...props
+}: ButtonProps): JSX.Element => {
+  const { background, shadow } = variants[variant];
+
   return (
     <button
       {...props}
-      className={`min-h-[2rem] min-w-[8rem] max-w-full scale-100 overflow-x-hidden text-ellipsis whitespace-nowrap bg-brand-red p-2 font-display font-medium text-white shadow-md shadow-brand-red transition-all hover:scale-90 hover:shadow-none ${props.className}`}
+      className={`min-h-[2rem] min-w-[8rem] max-w-full scale-100 overflow-x-hidden text-ellipsis whitespace-nowrap p-2 font-display font-medium text-white shadow-md transition-all hover:scale-90 hover:shadow-none ${background} ${
+        !noShadow && shadow
+      } ${props.className}`}
     >
       {children}
     </button>
